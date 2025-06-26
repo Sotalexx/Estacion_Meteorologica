81a75504a6f37e4fd1691799bd359c0481e8caa8
BeatrizzJimenezz <cj21004@ues.edu.sv>
2025-06-25 22:06:01 -0600

    Termino diseño del main

--- app/src/main/java/com/example/estacion_meteorologica/MainActivity.java
+++ app/src/main/java/com/example/estacion_meteorologica/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.estacion_meteorologica;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
+import antonkozyriatskyi.circularprogressindicator.CircularProgressIndicator;
+
 public class MainActivity extends AppCompatActivity {
+
+/*    LineChart chartTemperatura;*/
+    private CircularProgressIndicator gaugeHumedad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,57 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        CircularProgressIndicator circularProgress = findViewById(R.id.gaugePresionMar);
+
+        gaugeHumedad = findViewById(R.id.gaugePresionLocal);
+
+        // Establece el valor de humedad
+        float humedadActual = 88;
+        float humedad = 130;
+        gaugeHumedad.setProgress(humedadActual, humedad);
+
+// you can set max and current progress values individually
+        circularProgress.setMaxProgress(100);
+        circularProgress.setCurrentProgress(88);
+// or all at once
+        circularProgress.setProgress(100, 130);
+
+// you can get progress values using following getters
+        circularProgress.getProgress(); // returns 5000
+        circularProgress.getMaxProgress(); // returns 10000
+
+/*        chartTemperatura = findViewById(R.id.chartTemperatura);
+        simularGrafica();*/
+
+
     }
+
+
+/*    private void simularGrafica() {
+        ArrayList<Entry> entradas = new ArrayList<>();
+        entradas.add(new Entry(0, 26f));
+        entradas.add(new Entry(1, 26f));
+        entradas.add(new Entry(2, 25f));
+        entradas.add(new Entry(3, 24f));
+        entradas.add(new Entry(4, 24f));
+
+        LineDataSet dataSet = new LineDataSet(entradas, "Temperatura");
+        dataSet.setColor(Color.YELLOW);
+        dataSet.setCircleColor(Color.WHITE);
+        dataSet.setLineWidth(2f);
+        dataSet.setValueTextColor(Color.WHITE);
+        dataSet.setDrawFilled(true);
+        dataSet.setFillColor(Color.parseColor("#FFBB33"));
+        dataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(dataSet);
+        chartTemperatura.setData(lineData);
+        chartTemperatura.getDescription().setEnabled(false);
+        chartTemperatura.getLegend().setEnabled(false);
+
+        chartTemperatura.invalidate(); // Actualizar gráfico
+    }*/
+
+
 }
