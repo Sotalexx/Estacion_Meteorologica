8ef6743a7f4a71464215345a4c5a8f39742bf4c7
BeatrizzJimenezz <cj21004@ues.edu.sv>
2025-06-27 13:55:04 -0600

    Cambios de graficas en el main

--- app/src/main/java/com/example/estacion_meteorologica/MainActivity.java
+++ app/src/main/java/com/example/estacion_meteorologica/MainActivity.java
@@ -12,26 +12,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
+import com.ekn.gruzer.gaugelibrary.ArcGauge;
+import com.ekn.gruzer.gaugelibrary.HalfGauge;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import antonkozyriatskyi.circularprogressindicator.CircularProgressIndicator;
+import com.ekn.gruzer.gaugelibrary.Range;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference estacionRef;
-
-/*    LineChart chartTemperatura;*/
-    private CircularProgressIndicator gaugeHumedad;
+    BarChart barChart;
+    private CircularProgressIndicator gaugePresionL, gaugePresionMar, gaugeHumedad;
+    private HalfGauge gaugeViento;
+    private ArcGauge gaugeIndiceCalor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,102 +61,46 @@ public class MainActivity extends AppCompatActivity {
         leerUltimoRegistro();
 
 
+        gaugePresionMar = findViewById(R.id.gaugePresionMar);
+        gaugePresionL = findViewById(R.id.gaugePresionLocal);
+        gaugeHumedad = findViewById(R.id.gaugeHumedad);
+        gaugeViento = findViewById(R.id.gaugeVientos);
+        gaugeIndiceCalor = findViewById(R.id.gaugeIndiceCalor);
 
-        CircularProgressIndicator circularProgress = findViewById(R.id.gaugePresionMar);
-
-        gaugeHumedad = findViewById(R.id.gaugePresionLocal);
-
-        // Establece el valor de humedad
-        float humedadActual = 88;
-        float humedad = 130;
-        gaugeHumedad.setProgress(humedadActual, humedad);
-
-// you can set max and current progress values individually
-        circularProgress.setMaxProgress(100);
-        circularProgress.setCurrentProgress(88);
-// or all at once
-        circularProgress.setProgress(100, 130);
-
-// you can get progress values using following getters
-        circularProgress.getProgress(); // returns 5000
-        circularProgress.getMaxProgress(); // returns 10000
-
-/*        chartTemperatura = findViewById(R.id.chartTemperatura);
-        simularGrafica();*/
-
+        barChart = findViewById(R.id.barChart);
 
     }
 
+    class RegistroClima {
+        String fecha;
+        String hora;
+        Double temperatura;
+        Double humedad;
+        Double sensacionTermica;
+        Double presionLocal;
+        Double presionMar;
+        Double altitud;
+        Double viento;
+        Integer lluvia;
+        Integer gas;
+        Integer tendencia;
+        Integer monoxido;
+        Integer humo;
+        Integer rocio;
+        Integer indiceCalor;
+        Integer humedadSuelo;
+    }
 
     private void leerUltimoRegistro() {
         estacionRef.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot datoSnapshot : snapshot.getChildren()) {
-                    String fecha = datoSnapshot.child("fecha").getValue(String.class);
-                    String hora = datoSnapshot.child("hora").getValue(String.class);
-                    Double temperatura = datoSnapshot.child("temperatura").getValue(Double.class);
-                    Double humedad = datoSnapshot.child("humedad").getValue(Double.class);
-                    Double sensacionTermica = datoSnapshot.child("sensacion_termica").getValue(Double.class);
-                    Double presionBarometrica = datoSnapshot.child("presion_local").getValue(Double.class);
-                    Double presionNivelMar = datoSnapshot.child("presion_nivel_mar").getValue(Double.class);
-                    Double altitud = datoSnapshot.child("altitud_calculada").getValue(Double.class);
-                    Double viento = datoSnapshot.child("viento_kmh").getValue(Double.class);
-                    Integer lluvia = datoSnapshot.child("lluvia").getValue(Integer.class);
-                    Integer gas = datoSnapshot.child("lpg_ppm").getValue(Integer.class);
-                    Integer tendencia = datoSnapshot.child("presion_tendencia").getValue(Integer.class);
-                    Integer monoxido = datoSnapshot.child("co_ppm").getValue(Integer.class);
-                    Integer humo = datoSnapshot.child("smoke_ppm").getValue(Integer.class);
-                    Integer rocio = datoSnapshot.child("punto_rocio").getValue(Integer.class);
-                    Integer indiceCalor = datoSnapshot.child("indice_calor").getValue(Integer.class);
-                    Integer sueloHumedad = datoSnapshot.child("suelo_humedad").getValue(Integer.class);
-
-                    TextView tvFecha = findViewById(R.id.tvDate);
-                    TextView tvHora = findViewById(R.id.tvTime);
-                    TextView tvTemperatura = findViewById(R.id.tvTemperature);
-                    TextView tvSensasion = findViewById(R.id.tvRealFeel);
-                    TextView tvLluvia = findViewById(R.id.tvRainProbability);
-                    TextView tvTemp = findViewById(R.id.tvTempCard1);
-                    TextView tvViento = findViewById(R.id.tvWindCard1);
-                    TextView tvHumedad = findViewById(R.id.tvHumedadCard1);
-                    TextView tvPresionL = findViewById(R.id.tvPresionLocal);
-                    TextView tvPresionM = findViewById(R.id.tvPresionMar);
-                    TextView tvAltitud = findViewById(R.id.tvAltitud);
-                    TextView tvTendencia = findViewById(R.id.tvTendenciaPresion);
-                    TextView tvGas = findViewById(R.id.tvGasLPG);
-                    TextView tvMonoxido = findViewById(R.id.tvMonoxido);
-                    TextView tvHumo = findViewById(R.id.tvHumo);
-                    TextView tvHumedadSuelo = findViewById(R.id.tvHumedadSuelo);
-                    TextView tvHumeda = findViewById(R.id.tvHumedad);
-                    TextView tvRocio = findViewById(R.id.tvPuntoRocio);
-                    TextView tvIndice = findViewById(R.id.tvIndiceCalor);
-                    TextView tvVient = findViewById(R.id.tvViento);
-
-
-                    tvFecha.setText(fecha);
-                    tvHora.setText(hora);
-                    tvTemp.setText(temperatura + "°C");
-                    tvTemperatura.setText(temperatura + "°C");
-                    tvSensasion.setText(sensacionTermica + "°C");
-                    tvLluvia.setText(lluvia + "%");
-                    tvViento.setText(viento + " km/h");
-                    tvVient.setText(viento + " km/h");
-                    tvLluvia.setText(lluvia + "%");
-                    tvHumedad.setText(humedad + "%");
-                    tvPresionL.setText(presionBarometrica + " hPa");
-                    tvPresionM.setText(presionNivelMar + " hPa");
-                    tvTendencia.setText(tendencia + " hPa");
-                    tvGas.setText(gas + " ppm");
-                    tvMonoxido.setText(monoxido + " ppm");
-                    tvHumo.setText(humo + " ppm");
-                    tvHumedadSuelo.setText(sueloHumedad + " %");
-                    tvHumeda.setText(humedad + " %");
-                    tvRocio.setText(rocio + "°");
-                    tvIndice.setText(String.valueOf(indiceCalor));
-                    tvAltitud.setText(altitud + " m");
-
-
-
+                    RegistroClima registro = obtenerDatosDeSnapshot(datoSnapshot);
+                    mostrarDatosEnTextViews(registro);
+                    mostrarGraficas(registro);
+                    configurarGaugeViento(registro.viento);
+                    configurarGaugeIndiceCalor(registro.indiceCalor);
                 }
             }
 
@@ -156,34 +111,160 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private RegistroClima obtenerDatosDeSnapshot(DataSnapshot snapshot) {
+        RegistroClima r = new RegistroClima();
+        r.fecha = snapshot.child("fecha").getValue(String.class);
+        r.hora = snapshot.child("hora").getValue(String.class);
+        r.temperatura = snapshot.child("temperatura").getValue(Double.class);
+        r.humedad = snapshot.child("humedad").getValue(Double.class);
+        r.sensacionTermica = snapshot.child("sensacion_termica").getValue(Double.class);
+        r.presionLocal = snapshot.child("presion_local").getValue(Double.class);
+        r.presionMar = snapshot.child("presion_nivel_mar").getValue(Double.class);
+        r.altitud = snapshot.child("altitud_calculada").getValue(Double.class);
+        r.viento = snapshot.child("viento_kmh").getValue(Double.class);
+        r.lluvia = snapshot.child("lluvia_porcentaje").getValue(Integer.class);
+        r.gas = snapshot.child("lpg_ppm").getValue(Integer.class);
+        r.tendencia = snapshot.child("presion_tendencia").getValue(Integer.class);
+        r.monoxido = snapshot.child("co_ppm").getValue(Integer.class);
+        r.humo = snapshot.child("smoke_ppm").getValue(Integer.class);
+        r.rocio = snapshot.child("punto_rocio").getValue(Integer.class);
+        r.indiceCalor = snapshot.child("indice_calor").getValue(Integer.class);
+        r.humedadSuelo = snapshot.child("suelo_humedad").getValue(Integer.class);
+        return r;
+    }
 
+    private void mostrarDatosEnTextViews(RegistroClima r) {
+        TextView tvFecha = findViewById(R.id.tvDate);
+        TextView tvHora = findViewById(R.id.tvTime);
+        TextView tvTemperatura = findViewById(R.id.tvTemperature);
+        TextView tvSensasion = findViewById(R.id.tvRealFeel);
+        TextView tvLluvia = findViewById(R.id.tvRainProbability);
+        TextView tvTemp = findViewById(R.id.tvTempCard1);
+        TextView tvViento = findViewById(R.id.tvWindCard1);
+        TextView tvHumedad = findViewById(R.id.tvHumedadCard1);
+        TextView tvPresionL = findViewById(R.id.tvPresionLocal);
+        TextView tvPresionM = findViewById(R.id.tvPresionMar);
+        TextView tvAltitud = findViewById(R.id.tvAltitud);
+        TextView tvTendencia = findViewById(R.id.tvTendenciaPresion);
+        TextView tvGas = findViewById(R.id.tvGasLPG);
+        TextView tvMonoxido = findViewById(R.id.tvMonoxido);
+        TextView tvHumo = findViewById(R.id.tvHumo);
+        TextView tvHumedadSuelo = findViewById(R.id.tvHumedadSuelo);
+        TextView tvHumeda = findViewById(R.id.tvHumedad);
+        TextView tvRocio = findViewById(R.id.tvPuntoRocio);
+        TextView tvIndice = findViewById(R.id.tvIndiceCalor);
+        TextView tvVient = findViewById(R.id.tvViento);
 
-}
+        SimpleDateFormat entradaFecha = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        SimpleDateFormat salidaFecha = new SimpleDateFormat("dd MMMM, yyyy", new Locale("es", "ES"));
+        SimpleDateFormat entradaHora = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        SimpleDateFormat salidaHora = new SimpleDateFormat("hh:mm a", Locale.getDefault());
 
+        try {
+            Date fecha = entradaFecha.parse(r.fecha);
+            tvFecha.setText(salidaFecha.format(fecha));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            tvFecha.setText(r.fecha);
+        }
 
-/*    private void simularGrafica() {
-        ArrayList<Entry> entradas = new ArrayList<>();
-        entradas.add(new Entry(0, 26f));
-        entradas.add(new Entry(1, 26f));
-        entradas.add(new Entry(2, 25f));
-        entradas.add(new Entry(3, 24f));
-        entradas.add(new Entry(4, 24f));
+        try {
+            Date hora = entradaHora.parse(r.hora);
+            tvHora.setText(salidaHora.format(hora));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            tvHora.setText(r.hora);
+        }
 
-        LineDataSet dataSet = new LineDataSet(entradas, "Temperatura");
-        dataSet.setColor(Color.YELLOW);
-        dataSet.setCircleColor(Color.WHITE);
-        dataSet.setLineWidth(2f);
-        dataSet.setValueTextColor(Color.WHITE);
-        dataSet.setDrawFilled(true);
-        dataSet.setFillColor(Color.parseColor("#FFBB33"));
+        tvTemp.setText(r.temperatura + "°C");
+        tvTemperatura.setText(r.temperatura + "°C");
+        tvSensasion.setText(r.sensacionTermica + "°C");
+        tvLluvia.setText("Lluvia: " + r.lluvia + "%");
+        tvViento.setText(r.viento + " km/h");
+        tvVient.setText(r.viento + " km/h");
+        tvHumedad.setText(r.humedad + "%");
+        tvPresionL.setText(r.presionLocal + " hPa");
+        tvPresionM.setText(r.presionMar + " hPa");
+        tvTendencia.setText(r.tendencia + " hPa");
+        tvGas.setText(r.gas + " ppm");
+        tvMonoxido.setText(r.monoxido + " ppm");
+        tvHumo.setText(r.humo + " ppm");
+        tvHumedadSuelo.setText(r.humedadSuelo + " %");
+        tvHumeda.setText(r.humedad + " %");
+        tvRocio.setText(r.rocio + "°");
+        tvIndice.setText(String.valueOf(r.indiceCalor));
+        tvAltitud.setText(r.altitud + " m");
+    }
+
+    private void mostrarGraficas(RegistroClima r) {
+        float presionMax = 1084.8f;
+        float humedadMax = 100f;
+        gaugePresionL.setProgress(r.presionLocal, presionMax);
+        gaugePresionMar.setProgress(r.presionMar, presionMax);
+        gaugeHumedad.setProgress(r.humedad, humedadMax);
+
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, r.temperatura.floatValue()));
+        entries.add(new BarEntry(1, r.rocio.floatValue()));
+
+        BarDataSet dataSet = new BarDataSet(entries, "");
+        dataSet.setColors(new int[]{0xFF348DF1, 0xFF00BCD4});
         dataSet.setDrawValues(false);
 
-        LineData lineData = new LineData(dataSet);
-        chartTemperatura.setData(lineData);
-        chartTemperatura.getDescription().setEnabled(false);
-        chartTemperatura.getLegend().setEnabled(false);
+        BarData barData = new BarData(dataSet);
+        barChart.setData(barData);
+        barChart.setTouchEnabled(false);
+        barChart.setDragEnabled(false);
+        barChart.setScaleEnabled(false);
+        barChart.setHighlightPerTapEnabled(false);
+        barChart.getXAxis().setEnabled(false);
 
-        chartTemperatura.invalidate(); // Actualizar gráfico
-    }*/
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setDrawLabels(false);
+        leftAxis.setDrawAxisLine(false);
+        leftAxis.setDrawGridLines(false);
+        barChart.getAxisRight().setEnabled(false);
 
+        barChart.getLegend().setEnabled(false);
+        Description description = new Description();
+        description.setText("");
+        barChart.setDescription(description);
+        barChart.setFitBars(true);
+        barChart.invalidate();
+    }
 
+    private void configurarGaugeViento(Double viento) {
+        agregarRangoGauge(gaugeViento, 0.0, 5.0, "#00BCD4");
+        agregarRangoGauge(gaugeViento, 5.0, 25.0, "#348DF1");
+        agregarRangoGauge(gaugeViento, 25.0, 38.0, "#3F51B5");
+        agregarRangoGauge(gaugeViento, 38.0, 60.0, "#B71C1C");
+        gaugeViento.setMinValue(0.0);
+        gaugeViento.setMaxValue(60.0);
+        gaugeViento.setValue(viento);
+    }
+
+    private void configurarGaugeIndiceCalor(Integer indice) {
+        agregarRangoGauge(gaugeIndiceCalor, 0.0, 27.0, "#4CAF50");
+        agregarRangoGauge(gaugeIndiceCalor, 27.0, 32.0, "#FFEB3B");
+        agregarRangoGauge(gaugeIndiceCalor, 32.0, 39.0, "#FF9800");
+        agregarRangoGauge(gaugeIndiceCalor, 39.0, 51.0, "#F1A134");
+        agregarRangoGauge(gaugeIndiceCalor, 51.0, 60.0, "#B71C1C");
+        gaugeIndiceCalor.setMinValue(0.0);
+        gaugeIndiceCalor.setMaxValue(60.0);
+        gaugeIndiceCalor.setValue(indice);
+    }
+
+    private void agregarRangoGauge(Object gauge, double from, double to, String colorHex) {
+        Range rango = new Range();
+        rango.setFrom(from);
+        rango.setTo(to);
+        rango.setColor(Color.parseColor(colorHex));
+
+        if (gauge instanceof HalfGauge) {
+            ((HalfGauge) gauge).addRange(rango);
+        } else if (gauge instanceof ArcGauge) {
+            ((ArcGauge) gauge).addRange(rango);
+        }
+    }
+
+}
